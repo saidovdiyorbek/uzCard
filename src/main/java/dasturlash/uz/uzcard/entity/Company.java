a1c209b21fa648567713c5af26cf302c8be05061
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-13 22:36:40 +0500

    Added Company

--- src/main/java/dasturlash/uz/uzcard/entity/Company.java
+++ src/main/java/dasturlash/uz/uzcard/entity/Company.java
@@ -1,4 +1,24 @@
 package dasturlash.uz.uzcard.entity;
 
+import dasturlash.uz.uzcard.enums.CompanyRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "company")
+@Data
 public class Company {
+    private String id;
+    private String name;
+    private Address address;
+    private String contact;
+    private LocalDateTime createdAt;
+    private Boolean visible;
+    private CompanyRole role;
+    private String code;
+    private String username;
+    private String password;
 }
