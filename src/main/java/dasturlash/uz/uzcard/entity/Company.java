0f8499cce619368c9ce7b8058454ba5c6f823bb7
saidovdiyorbek <saidovdiyer777@gmail.com>
2024-12-14 16:46:15 +0500

    Added: Company

--- src/main/java/dasturlash/uz/uzcard/entity/Company.java
+++ src/main/java/dasturlash/uz/uzcard/entity/Company.java
@@ -1,8 +1,8 @@
 package dasturlash.uz.uzcard.entity;
 
+import dasturlash.uz.uzcard.entity.address.Address;
 import dasturlash.uz.uzcard.enums.CompanyRole;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -11,14 +11,38 @@ import java.time.LocalDateTime;
 @Table(name = "company")
 @Data
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @Column(name = "address_id", unique = true, nullable = false)
+    private String addressId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id", insertable = false, updatable = false)
     private Address address;
+
+    @Column(nullable = false)
     private String contact;
+
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    @Column(nullable = false)
     private Boolean visible;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private CompanyRole role;
+
+    @Column(nullable = false)
     private String code;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 }
